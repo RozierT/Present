cd20af6122f314384222e8baba64705cfc678fea
RozierT <135091431+RozierT@users.noreply.github.com>
2023-11-15 19:47:28 -0800

    initial commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -14,24 +13,22 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
-
-  app.use(express.urlencoded({ extended: false }));
+  
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server));
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
-
+  // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
-  }
+  } 
 
   db.once('open', () => {
     app.listen(PORT, () => {
@@ -41,5 +38,4 @@ const startApolloServer = async () => {
   });
 };
 
-// Call the async function to start the server
-  startApolloServer();
+startApolloServer();
