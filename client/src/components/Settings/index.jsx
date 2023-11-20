52acc4a596806446c6dea6dba4a5aa482d9fde2e
RozierT <135091431+RozierT@users.noreply.github.com>
2023-11-20 00:44:35 -0800

    refactored the setting page to map the array of setting and be more modular

--- client/src/components/Settings/index.jsx
+++ client/src/components/Settings/index.jsx
@@ -1,81 +1,93 @@
-import React, { Component } from 'react';
-import Toggle from './Toggle';
+import React, { Component } from "react";
+import Toggle from "./Toggle";
+
+const SettingsList = [
+  {
+    id: 1,
+    label: "Theme",
+    labelLeft: "Dark",
+    labelRight: "Light",
+    value: "settings.theme",
+    name: "theme",
+    checked: "settings.theme === light",
+  },
+  {
+    id: 2,
+    label: "Notifications",
+    labelLeft: "On",
+    labelRight: "Off",
+    value: "settings.notifications",
+    name: "notifications",
+    checked: "settings.notifications === Off",
+  },
+  {
+    id: 3,
+    label: "Visibility",
+    labelLeft: "Public",
+    labelRight: "Private",
+    value: "settings.visibility",
+    name: "visibility",
+    checked: "settings.visibility === private",
+  },
+];
 
 class Settings extends Component {
- constructor(props) {
-   super(props);
-   this.state = {
-     settings: {
-       theme: 'light',
-       notifications: true,
-       visibility: 'public'
-     }
-   };
- }
+  constructor(props) {
+    super(props);
+    this.state = {
+      settings: {
+        theme: "light",
+        notifications: true,
+        visibility: "public",
+      },
+    };
+  }
 
- handleChange = (event) => {
-   const { name, value } = event.target;
-   this.setState(prevState => ({
-     settings: {
-       ...prevState.settings,
-       [name]: value
-     }
-   }));
- }
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState((prevState) => ({
+      settings: {
+        ...prevState.settings,
+        [name]: value,
+      },
+    }));
+  };
 
- render() {
-   const { settings } = this.state;
-   
-   return (
-     <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'
-     }}>
-       <h1 className='font-bold'>Settings</h1>
-       <br></br>
-       <form>
-         <label>
-           Theme:
-           <Toggle 
-           labelLeft={' Dark '}
-            labelRight={' Light '}
-            value={settings.theme}
-            onChange={this.handleChange}
-            name="theme"
-            checked={settings.theme === 'light'}
-           />
-         </label>
-         <br></br>
-         <label>
-           Notifications:
-           <Toggle 
-           labelLeft={' On '}
-            labelRight={' Off '}
-            value={settings.notifications}
-            onChange={this.handleChange}
-            name="notifications"
-            checked={settings.notifications === 'Off'}
-           />
-         </label>
-         <br></br>
-         <label>
-           Visibility:
-           <Toggle 
-           labelLeft={' Public '}
-            labelRight={' Private '}
-            value={settings.visibility}
-            onChange={this.handleChange}
-            name="visibility"
-            checked={settings.visibility === 'private'}
-           />
-         </label>
-       </form>
-     </div>
-   );
- }
+  render() {
+    const { settings } = this.state;
+
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <h1 className="font-bold">Settings</h1>
+        <br></br>
+        <form>
+          {SettingsList.map((setting) => (
+            <div>
+              {setting.label}
+              <Toggle
+                key={setting.id}
+                label={setting.label}
+                labelLeft={setting.labelLeft}
+                labelRight={setting.labelRight}
+                value={setting.value}
+                onChange={this.handleChange}
+                name={setting.name}
+                checked={setting.checked}
+              />{"------------------------"}<br></br>
+            </div>
+          ))}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Settings;
