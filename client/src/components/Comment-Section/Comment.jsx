4dc64568c3dee475fa2502eb7086e414c031f62e
Hamza Lhamous <hamza.lhamous@gmail.com>
2023-11-20 17:22:38 -0800

    files made

---
+++ client/src/components/Comment-Section/Comment.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ImageIcon from "../profile/ImageIcon";
+
+// in this component there should be the user who posted the image on the top with their profile pic username and their caption
+// underneath there should be a prompt that says add a comment
+// if there are comments already they should be rendered with the commenters profile pic, username, and their comment
+// each comment at the top layer should have a reply prompt, and should be expanded when clicked on to render all the replies
+// the simplest component would be a profile pic a username and text
