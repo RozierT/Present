494d587bdac02de31589309bb06024bbf4bcaf38
Sier Behashti <sbehashti@gmail.com>
2023-11-28 23:08:40 -0800

    Added Mutation for follow and unfollow

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,28 +1,30 @@
 const { User, Post, Comment, Profile } = require("../models");
-const { signToken, AuthenticationError } = require('../utils/auth')
-
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
-
   Query: {
     // Find (logged in) user by id (context) and return their Flair Score Array
-    userPrefs: async ( parent, args, context) => {
-
+    userPrefs: async (parent, args, context) => {
       if (!context.user) {
-        throw new Error('Authentication required');
+        throw new Error("Authentication required");
       }
 
-      const userPrefs = await User.findById(context.user._id).select('flairScores')
+      const userPrefs = await User.findById(context.user._id).select(
+        "flairScores"
+      );
 
       // console.log('userPrefs: ', userPrefs)
 
-      return userPrefs.flairScores
+      return userPrefs.flairScores;
     },
     // Find posts in db that match passed in params and return the ids (array)
-    getWeightedPosts: async ( parent, { flair, recencyScore, dateRange }, context ) => {
-
+    getWeightedPosts: async (
+      parent,
+      { flair, recencyScore, dateRange },
+      context
+    ) => {
       if (!context.user) {
-        throw new Error('Authentication required');
+        throw new Error("Authentication required");
       }
 
       let startDate, endDate;
@@ -37,72 +39,68 @@ const resolvers = {
       }
 
       const weightedPosts = await Post.find({
-        flairs: { $in: [flair] }, 
+        flairs: { $in: [flair] },
         recencyScore: recencyScore,
         dateCreated: {
-          $gte: startDate, 
-          $lte: endDate 
-        }
-      })
-      .select('_id')
+          $gte: startDate,
+          $lte: endDate,
+        },
+      }).select("_id");
 
-      return weightedPosts
+      return weightedPosts;
     },
     // find post by id that is passed in thru args
     getPostById: async (parent, args, context) => {
-
       // if (!context.user) {
       //   throw new Error('Authentication required');
       // }
 
-      const queriedPost = await Post.findById(args._id)
+      const queriedPost = await Post.findById(args._id);
 
-      return queriedPost
+      return queriedPost;
     },
     getPostsById: async (parent, args, context) => {
+      console.log("args: ", args);
 
-      console.log('args: ', args)
-      
       if (!context.user) {
-        throw new Error('Authentication required');
+        throw new Error("Authentication required");
       }
 
       const queriedPosts = await Post.find({
-        _id: { $in: args.ids }
-      })
+        _id: { $in: args.ids },
+      });
 
-      console.log('queried Posts: ', queriedPosts)
+      console.log("queried Posts: ", queriedPosts);
 
-      return queriedPosts
+      return queriedPosts;
     },
     me: async (parent, args, context) => {
-
       if (!context.user) {
-        throw new Error('Authentication required');
+        throw new Error("Authentication required");
       }
 
-      console.log('user id: ', context.user._id)
+      console.log("user id: ", context.user._id);
 
-      const thisUser = await Profile.findById(context.user._id).populate('posts')
+      const thisUser = await Profile.findById(context.user._id).populate(
+        "posts"
+      );
 
-      return thisUser
+      return thisUser;
     },
     getOthersProfile: async (parent, args, context) => {
-
       if (!context.user) {
-        throw new Error('Authentication required');
+        throw new Error("Authentication required");
       }
 
-      console.log('args: ', args)
-      console.log('userId: ', args.userId)
+      console.log("args: ", args);
+      console.log("userId: ", args.userId);
 
       const othersProlie = await Profile.find({
-        userId: args.userId
-      })
-      .populate('posts')
+        userId: args.userId,
+      }).populate("posts");
 
-      return othersProlie
-    }
+      return othersProlie;
+    },
   },
   Mutation: {
     login: async (parent, { email, password }) => {
@@ -123,71 +121,95 @@ const resolvers = {
       return { token, user };
     },
     addUser: async (parent, { firstName, lastName, email, password }) => {
-      
       const user = await User.create({ firstName, lastName, email, password });
-      
+
       const token = signToken(user);
 
       return { token, user };
     },
     addProfile: async (parent, { username, bio, profilePicture }, context) => {
-
       if (!context.user) {
-        throw new Error('Authentication required');
+        throw new Error("Authentication required");
       }
 
       const profile = await Profile.create({
         username,
         bio,
         profilePicture,
-        userId: context.user._id
+        userId: context.user._id,
       });
 
       return profile;
     },
     // Used to update a user's flairScores array after Profile creation
     updateUserPrefs: async (parent, args, context) => {
-
-      const { input } = args
+      const { input } = args;
 
       if (!context.user) {
-        throw new Error('Authentication required');
+        throw new Error("Authentication required");
       }
 
       const updatedUser = await User.findOneAndUpdate(
         { _id: context.user._id },
         { $set: { flairScores: input } },
         { new: true }
-      )
+      );
 
       if (!updatedUser) {
-        throw new Error('User not found');
+        throw new Error("User not found");
       }
 
-      return updatedUser
+      return updatedUser;
     },
     // creates post based on passed in data then updates profile with new post id
-    createPost: async (parent, { content, textContent, flairs}, context) => {
-
+    createPost: async (parent, { content, textContent, flairs }, context) => {
       if (!context.user) {
-        throw new Error('Authentication required');
+        throw new Error("Authentication required");
       }
 
       const createdPost = await Post.create({
         content,
         textContent,
-        flairs
-      })
+        flairs,
+      });
 
       if (createdPost) {
         const updatedProfile = await Profile.findOneAndUpdate(
           { userId: context.user._id },
           { $push: { posts: createdPost._id } },
-          { new: true },
-        )
+          { new: true }
+        );
 
-        return { createdPost, updatedProfile }
+        return { createdPost, updatedProfile };
       }
+    },
+    // Following a user
+    followUser: async (parent, { followUserId }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You must be logged in!");
+      }
+
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { following: followUserId } },
+        { new: true }
+      );
+
+      return updatedUser;
+    },
+    // Un-following a user
+    unFollowUser: async (parent, { unFollowUserId }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You must be logged in!");
+      }
+
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { following: unfollowUserId } },
+        { new: true }
+      );
+
+      return updatedUser;
     },
   },
 };
